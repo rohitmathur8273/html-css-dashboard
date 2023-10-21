1fb4a9652464e219d69c223ebc03855739ff9fcd
shyam sundar <79298661+rohitmathur8273@users.noreply.github.com>
2023-10-22 00:31:55 +0530

    files commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// const dashboardNav= document.querySelector('#dashboard-nav');
+
+// dashboardNav.addEventListener('click', ()=>{
+//     console.log('dashboard is click')
+// })
+
+function goToDashboard () {
+    console.log('hariram')
+}
